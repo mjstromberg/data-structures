1a8d981561a4a006e20db9fc54a7bf3c525f74a3
mjstromberg <michaeljstromberg@gmail.com>
2016-09-14 14:57:48 -0700

    Implement queue using functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    var result = storage[0];
+    counter = counter > 0 ? counter-1 : 0;
+    delete storage[0];
+    for (var key in storage) {
+      storage[+key-1] = storage[key];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
