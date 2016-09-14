139304c0e8a0875c8d9df40191c2c15ec8d74ada
mjstromberg <michaeljstromberg@gmail.com>
2016-09-14 16:42:02 -0700

    Start implementing pseudo classical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,15 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+};
+
+Stack.prototype.push = function() {
+  return this.counter++;
+};
+
+Stack.prototype.size = function() {
+  return 0;
 };
 
 
