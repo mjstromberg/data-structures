6213cfd3cc9fe3425e240e95cdd182da472eeaa0
mjstromberg <michaeljstromberg@gmail.com>
2016-09-14 16:37:38 -0700

    Implement both prototypical patterns

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.counter = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++; 
+  },
+  dequeue: function() {
+    var result = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    this.counter = this.counter > 0 ? this.counter - 1 : 0;
+    return result;
+  },
+  size: function() {
+    return this.counter;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.counter = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+  pop: function() {
+    var result = this.storage[this.counter - 1];
+    delete this.storage[this.counter - 1];
+    this.counter = this.counter > 0 ? this.counter - 1 : 0;
+    return result;
+  },
+  size: function() {
+    return this.counter;
+  }
+};
 
 
