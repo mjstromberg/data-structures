559cf64f079e6b1c335d58ae93d1e6c01ea15e20
mjstromberg <michaeljstromberg@gmail.com>
2016-09-15 16:40:26 -0700

    Start hashTable.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,34 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var collision = this._storage.get(index);
+  if (collision !== undefined) {
+    var array = [];
+    array.concat(collision, [k, v]);
+    this._storage.set(index, array);
+  } else {
+    this._storage.set(index, v);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var collisionCheck = this._storage.get(index);
+  if (Array.isArray(collisionCheck)) {
+    var found = null;
+    collisionCheck.forEach(function(array) {
+      if (array[0] === k) {
+        found = v;
+      }
+    });
+    return found;
+  }
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
