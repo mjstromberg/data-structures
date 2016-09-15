665446e59127e62c8e83dfef615bfe390c1ea975
mjstromberg <michaeljstromberg@gmail.com>
2016-09-15 15:18:16 -0700

    Implement graph using pseudo-classical.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,52 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = {value: node};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return _.map(this.storage, function(item) {
+    return item.value;
+  }).indexOf(node) > -1 ? true : false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.storage[node].edge) {
+    var edgeNode = this.storage[node].edge;
+    this.storage[edgeNode].edge = null;
+  }
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.storage[fromNode].edge === toNode;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode].edge = toNode;
+  this.storage[toNode].edge = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this.storage[fromNode].edge = null;
+  this.storage[toNode].edge = null;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.storage) {
+    cb(this.storage[key].value);
+  }
 };
 
 /*
