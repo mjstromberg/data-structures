64f856a1222fdcc6454fb7bd729cf544cc72aee7
mjstromberg <michaeljstromberg@gmail.com>
2016-09-15 13:55:19 -0700

    Implement Tree using functional-shared class instantiation.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,39 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  // children property, an array containing a number of subtrees
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
-
+//takes any value, sets that as the target of a node, and adds that node as a child of the tree
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
-
+//takes any input and returns a boolean reflecting whether it 
+//can be found as the value of the target node or any descendant node
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  if (this.children.length > 0) {
+    var found = false;
+
+    this.children.forEach(function(child) {
+      if (child.value === target) {
+        found = true;
+      }
+      found = found || child.contains(target);
+    });
+
+    return found;
+  }
+
+  return false;
 };
 
 
