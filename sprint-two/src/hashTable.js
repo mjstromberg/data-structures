dd12f93ec058c4aa17d632597e4a7438e7c9ce68
mjstromberg <michaeljstromberg@gmail.com>
2016-09-16 12:11:01 -0700

    Refactor hashTable using nested arrays.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,55 +1,57 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
-
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var collision = this._storage.get(index);
-  if (collision !== undefined && collision[0] !== k) {
-    var newIndex = null;
-    for (var i = 0; i < this._limit; i++) {
-      if (this._storage.get(i) === undefined) {
-        newIndex = newIndex !== null ? newIndex : i;
-      }  
-    }
-    this._storage.set(newIndex, [k, v]);
+  var storageCopy = this._storage.get(index);
+  
+  if (storageCopy === undefined) {
+    this._storage.set(index, [[k, v]]);
   } else {
-    this._storage.set(index, [k, v]);
+    var kMatch = false;
+    var matchIndex;
+    for (var i = 0; i < storageCopy.length; i++) {
+      if (storageCopy[i][0] === k) {
+        kMatch = true;
+        matchIndex = i;
+      }
+    }
+    if (kMatch) {
+      storageCopy[matchIndex][1] = v;
+    } else { 
+      storageCopy.push([k, v]);
+    }
+    this._storage.set(index, storageCopy);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var result;
-  for (var i = 0; i < this._limit; i++) {   
-    var val = this._storage.get(i);
-    if (val !== undefined && val[0] === k) {
-      result = this._storage.get(i)[1];
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var storageCopy = this._storage.get(index);
+  for (var i = 0; i < storageCopy.length; i++) {
+    if (storageCopy[i][0] === k) {
+      result = storageCopy[i][1];
     }
   }
   return result;
 };
 
 HashTable.prototype.remove = function(k) {
-  for (var i = 0; i < this._limit; i++) {
-    var val = this._storage.get(i);
-    if (val !== undefined && val[0] === k) {
-      this._storage.set(i, undefined);
+
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var storageCopy = this._storage.get(index);
+  for (var i = 0; i < storageCopy.length; i++) {
+    if (storageCopy[i][0] === k) {
+      storageCopy.splice(i, 1);
     }
   }
-};
+}; 
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-//if collision
-  //find next untaken index
-    //assign this to collided index 
-    //use collided index to store this new key and value
-
