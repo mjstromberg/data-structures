b9dc27cf3e0e09c561b09ee88facfcbf65a3b90c
mjstromberg <michaeljstromberg@gmail.com>
2016-09-16 16:42:22 -0700

    Add new unit test.

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -36,4 +36,12 @@ describe('binarySearchTree', function() {
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3]);
   });
+
+  it('should always return value less than input value when accessing left\'s value', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(6);
+    expect(binarySearchTree.left.value < binarySearchTree.value).to.be.true;
+  });
 });
