c9e3ea395928469c19341a5fedafad81d5143172
mjstromberg <michaeljstromberg@gmail.com>
2016-09-16 20:52:09 -0700

    Refactor hashTable.js to double hash table size when 75% of limit reached.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,9 +1,11 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   var storageCopy = this._storage.get(index);
   
@@ -25,12 +27,36 @@ HashTable.prototype.insert = function(k, v) {
     }
     this._storage.set(index, storageCopy);
   }
+
+  this._count++;
+  
+  if (this._count / this._limit >= .75) {
+    this._count = 0;
+    var oldLimit = this._limit;
+    this._limit *= 2;
+    var tuples = [];
+
+    for (var bucketIndex = 0; bucketIndex < oldLimit; bucketIndex++) {
+      var bucket = this._storage.get(bucketIndex);
+      if (bucket) {
+        for (var tupleIndex = 0; tupleIndex < bucket.length; tupleIndex++) {
+          tuples.push(this._storage.get(bucketIndex)[tupleIndex]);
+        }
+      }
+    }
+    
+    this._storage = LimitedArray(this._limit);
+    var context = this;
+    tuples.forEach(function(array) {
+      context.insert(array[0], array[1]);
+    });
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var result;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var storageCopy = this._storage.get(index);
+  var storageCopy = this._storage.get(index) || [];
   for (var i = 0; i < storageCopy.length; i++) {
     if (storageCopy[i][0] === k) {
       result = storageCopy[i][1];
